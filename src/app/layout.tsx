aae28a24d38265756a65b22f5091578dd73431b6
namikimlab <namixkim@gmail.com>
2025-11-14 15:50:39 +0900

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 import Header from "@/components/Header";
@@ -15,12 +16,26 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const GA_MEASUREMENT_ID = "G-W44924G8M6";
+
   return (
     <html lang="ko">
       <body
         suppressHydrationWarning
         className="antialiased min-h-screen bg-background text-foreground font-sans"
       >
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
         <Header /> 
         <SearchHeader />
         <main className="mx-auto max-w-6xl px-2 py-2">{children}</main>
